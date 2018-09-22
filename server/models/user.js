cbae9a7393b3b9ddccab4139d4c96acfe6f51452
Sam Savant <sam_savant@twoscoretwo.com>
2018-09-22 13:43:32 -0400

    Add post todos route

---
+++ server/models/user.js
@@ -0,0 +1,15 @@
+//Model
+var mongoose = require('mongoose');
+
+
+//User Model
+var User = mongoose.model('User', {
+    emailAddress: {
+        type: String,
+        //required: true,
+        trim:true,
+        minlength: 1
+    }
+});
+
+module.exports = {User};
